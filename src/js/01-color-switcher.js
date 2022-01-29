bf9e1f445e9922f6644039270f71037de7fb4fdd
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-01-29 15:01:52 +0200

    finish first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+let timerId = null;
 
+const buttonStartRef = document.querySelector('[data-start]');
+const buttonStopRef = document.querySelector('[data-stop]');
+
+buttonStopRef.setAttribute('disabled', true);
+
+buttonStartRef.addEventListener('click', onClickStart);
+buttonStopRef.addEventListener('click', onClickStop);
+
+function onClickStart(e) { 
+    setBackgroundColor();
+    timerId = setInterval(setBackgroundColor, 1000);
+    buttonStartRef.setAttribute('disabled', true);
+    buttonStopRef.removeAttribute('disabled');
+}
+
+function setBackgroundColor() {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+}
+ 
+function onClickStop (e) {
+    buttonStartRef.removeAttribute('disabled');
+    clearInterval(timerId);
+    buttonStopRef.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
