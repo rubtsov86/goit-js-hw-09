490649bf320bd3c5b799854849a4ef67dfa1b9c7
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-01-29 21:23:48 +0200

    almost

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,61 @@
+import Notiflix from 'notiflix';
+
+const createdArrayPromise = [];
+
+
+const formRef = document.querySelector('.form');
+const inputDelayRef = document.querySelector('[name="delay"]');
+const inputStepRef = document.querySelector('[name="step"]');
+const inputAmountRef = document.querySelector('[name="amount"]');
+
+
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  
+  createArray(inputAmountRef.value);
+  createdArrayPromise.reduce((acc, delay) => {createPromise(acc, delay)}, 1);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((resolve, reject) => {
+  setTimeout( () => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
+});
+  
+  return promise.then(
+    value => {
+      Notiflix.Notify.success(value); // "Success! Value passed to resolve function"
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error);
+    }); 
+
 }
+
+
+
+
+
+function createArray(number) {
+  let delayPromise = Number(inputDelayRef.value);
+
+  for (let i = 0; i < number; i += 1) {
+    createdArrayPromise.push(delayPromise);
+    delayPromise += Number(inputStepRef.value);
+  }
+
+  return createdArrayPromise;
+}
+
+
